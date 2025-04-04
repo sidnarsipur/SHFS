c128a118ef73d6e005445cd34d4550fdb01d685f
Henry Liu <codicate@gmail.com>
2025-04-04 14:57:08 -0400

    folder structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use clap::{Parser, Subcommand};
+mod client;
+mod naming;
+mod storage;
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Naming {},
+    Storage {},
+    Client {},
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Some(Commands::Naming {}) => naming::run(),
+        Some(Commands::Storage {}) => storage::run(),
+        Some(Commands::Client {}) => client::run(),
+        None => println!("Welcome to DFS. Use -h to see usage."),
+    };
 }

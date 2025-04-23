09d04a54251b7bef8f95080cc2dacea14a659e48
sidnarsipur <sid.narsipur@gmail.com>
2025-04-22 20:10:01 -0400

    change naming sever whitespace

--- src/naming/naming_service_impl.h
+++ src/naming/naming_service_impl.h
@@ -1,96 +1,84 @@
 #pragma once
 
 class NamingServiceImpl final : public NamingService::Service {
-public:
-    NamingServiceImpl() = default;
+    public:
+        NamingServiceImpl() = default;
 
-    grpc::Status RegisterStorageServer(grpc::ServerContext *context,
-                                       const RegisterStorageRequest *request,
-                                       RegisterStorageResponse *response) override {
-        std::unique_lock lock(mu_);
-        storage_servers_.insert(request->storage_address());
-        response->set_success(true);
-        return grpc::Status::OK;
-    }
-
-    grpc::Status GetStorageServersForFile(grpc::ServerContext *context,
-                                          const FileLookupRequest *request,
-                                          FileLookupResponse *response) {
-        std::shared_lock lock(mu_);
-        const auto &filepath = request->filepath();
-        if (file_locations_.contains(filepath)) {
-            for (const auto &addr: file_locations_[filepath]) {
-                response->add_storage_addresses(addr);
-            }
-        }
-        return grpc::Status::OK;
-    }
-
-    grpc::Status ListAllFiles(grpc::ServerContext *context,
-                              const Empty *request,
-                              FileListResponse *response) override {
-        std::shared_lock lock(mu_);
-        for (const auto &key: file_locations_ | std::views::keys) {
-            response->add_filepaths(key);
-        }
-        return grpc::Status::OK;
-    }
-
-    grpc::Status GetStorageServerForUpload(grpc::ServerContext *context,
-                                           const FileUploadRequest *request,
-                                           FileUploadResponse *response) {
-        std::unique_lock lock(mu_);
-        if (storage_servers_.empty()) {
-            return grpc::Status(grpc::StatusCode::FAILED_PRECONDITION, "No storage servers registered.");
-        }
-
-        // TODO: Add smarter selection strategy (e.g., round-robin, load-based)
-        auto it = storage_servers_.begin();
-        response->set_storage_address(*it);
-
-        // Update internal state
-        // TODO: handle updating a file, not just uploading new files
-        file_locations_[request->filepath()].insert(*it);
-
-        return grpc::Status::OK;
-    }
-
-    grpc::Status RemoveFile(grpc::ServerContext *context,
-                            const FileRemoveRequest *request,
-                            FileRemoveResponse *response) override {
-        std::unique_lock lock(mu_);
-        const auto &filename = request->filepath();
-        const auto it = file_locations_.find(filename);
-        if (it != file_locations_.end()) {
-            file_locations_.erase(it);
-            // TODO: Inform storage servers to delete file (distributed implementation)
+        grpc::Status RegisterStorageServer(grpc::ServerContext *context, const RegisterStorageRequest *request, RegisterStorageResponse *response) override {
+            std::unique_lock lock(mu_);
+            storage_servers_.insert(request->storage_address());
             response->set_success(true);
-        } else {
-            response->set_success(false);
+            return grpc::Status::OK;
         }
-        return grpc::Status::OK;
-    }
 
-    grpc::Status Heartbeat(grpc::ServerContext* context,
-                               const HeartbeatRequest* request,
-                               HeartbeatResponse* response) override {
-        std::string addr = request->address();
-        spdlog::info("Received heartbeat from: {}", addr);
+        grpc::Status GetStorageServersForFile(grpc::ServerContext *context, const FileLookupRequest *request, FileLookupResponse *response) {
+            std::shared_lock lock(mu_);
+            const auto &filepath = request->filepath();
+            if (file_locations_.contains(filepath)) {
+                for (const auto &addr: file_locations_[filepath]) {
+                    response->add_storage_addresses(addr);
+                }
+            }
+            return grpc::Status::OK;
+        }
 
-        // You could update an internal map of address -> last seen timestamp here
-        // serverState[addr] = std::chrono::steady_clock::now();
+        grpc::Status ListAllFiles(grpc::ServerContext *context, const Empty *request, FileListResponse *response) override {
+            std::shared_lock lock(mu_);
+            for (const auto &key: file_locations_ | std::views::keys) {
+                response->add_filepaths(key);
+            }
+            return grpc::Status::OK;
+        }
 
-        response->set_success(true);
-        response->set_message("Heartbeat received.");
-        return grpc::Status::OK;
-    }
+        grpc::Status GetStorageServerForUpload(grpc::ServerContext *context, const FileUploadRequest *request, FileUploadResponse *response) {
+            std::unique_lock lock(mu_);
+            if (storage_servers_.empty()) {
+                return grpc::Status(grpc::StatusCode::FAILED_PRECONDITION, "No storage servers registered.");
+            }
 
-private:
-    std::shared_mutex mu_; // For thread safety
+            // TODO: Add smarter selection strategy (e.g., round-robin, load-based)
+            auto it = storage_servers_.begin();
+            response->set_storage_address(*it);
 
-    // Set of registered storage server addresses
-    std::unordered_set<std::string> storage_servers_;
+            // Update internal state
+            // TODO: handle updating a file, not just uploading new files
+            file_locations_[request->filepath()].insert(*it);
 
-    // Map from filepaths to the set of storage servers that store them
-    std::unordered_map<std::string, std::unordered_set<std::string>> file_locations_;
+            return grpc::Status::OK;
+        }
+
+        grpc::Status RemoveFile(grpc::ServerContext *context, const FileRemoveRequest *request, FileRemoveResponse *response) override {
+            std::unique_lock lock(mu_);
+            const auto &filename = request->filepath();
+            const auto it = file_locations_.find(filename);
+            if (it != file_locations_.end()) {
+                file_locations_.erase(it);
+                // TODO: Inform storage servers to delete file (distributed implementation)
+                response->set_success(true);
+            } else {
+                response->set_success(false);
+            }
+            return grpc::Status::OK;
+        }
+
+        grpc::Status Heartbeat(grpc::ServerContext* context, const HeartbeatRequest* request, HeartbeatResponse* response) override {
+            std::string addr = request->address();
+            spdlog::info("Received heartbeat from: {}", addr);
+
+            // You could update an internal map of address -> last seen timestamp here
+            // serverState[addr] = std::chrono::steady_clock::now();
+
+            response->set_success(true);
+            response->set_message("Heartbeat received.");
+            return grpc::Status::OK;
+        }
+
+    private:
+        std::shared_mutex mu_; // For thread safety
+
+        // Set of registered storage server addresses
+        std::unordered_set<std::string> storage_servers_;
+
+        // Map from filepaths to the set of storage servers that store them
+        std::unordered_map<std::string, std::unordered_set<std::string>> file_locations_;
 };
